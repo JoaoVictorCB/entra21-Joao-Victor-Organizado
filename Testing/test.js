b74615315fde17e76f622de80ac9982693f1cc03
João Victor <joao.ervilhojv@gmail.com>
2021-07-05 17:12:09 -0300

    POO - Exercicios quase finalizados

---
+++ Testing/test.js
@@ -0,0 +1,12 @@
+class Agenda {
+    constructor(nome, telefone, indice) {
+        this.nome = nome
+        this.telefone = telefone
+        this.indice = indice
+    }
+  }
+
+  function armazenaPessoa(nome, telefone) {
+      let pessoa = nome, telefone
+      console.log(armazenaPessoa)
+  } 
