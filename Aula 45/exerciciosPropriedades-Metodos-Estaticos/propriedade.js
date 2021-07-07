57386eb9348003206b5f3b63af40c0f608694278
João Victor <joao.ervilhojv@gmail.com>
2021-07-07 17:17:15 -0300

    POO - Exercicios quase finalizados

--- Aula 45/exerciciosPropriedades-Metodos-Estaticos/propriedade.js
+++ Aula 45/exerciciosPropriedades-Metodos-Estaticos/propriedade.js
@@ -1,14 +1,14 @@
 // Exercício 1
 
-class Ponto2D {
-    constructor(x, y) {
-        this.x = x
-        this.y = y
-    }
-    temEixoComum(a, b) {
-        if (a == this.x || b == this.y) {
-            return true
-        }
-        return false
-    }
-}
+// class Ponto2D {
+//     constructor(x, y) {
+//         this.x = x
+//         this.y = y
+//     }
+//     temEixoComum(a, b) {
+//         if (a == this.x || b == this.y) {
+//             return true
+//         }
+//         return false
+//     }
+// }
